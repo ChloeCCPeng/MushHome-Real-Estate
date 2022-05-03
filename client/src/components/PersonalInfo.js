807751e7cf921bf738a9281fd5c34c87100bded2
Chloe <chloepeng331@icloud.com>
2022-05-03 00:13:55 -0400

    personal info

--- client/src/components/PersonalInfo.js
+++ client/src/components/PersonalInfo.js
@@ -2,7 +2,12 @@ import React from 'react'
 
 function PersonalInfo() {
   return (
-    <div>PersonalInfo</div>
+    <div>PersonalInfo
+      <h3>First Name: {user.firstName}</h3>
+      <h3>Last Name: {user.lastName}</h3>
+      <h3>Email: {user.email}</h3>
+      <h3>Contact Number: {user.phoneNumber}</h3>
+    </div>
   )
 }
 
