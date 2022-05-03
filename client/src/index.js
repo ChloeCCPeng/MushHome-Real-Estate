47396943ee745b9326d6cbca31f8fc180a9cf81c
Chloe <chloepeng331@icloud.com>
2022-05-03 10:19:06 -0400

    some updates

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+// import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter} from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(
-  <Router>
+// root.render(
+//   <Router>
+//     <App />
+//   </Router>,
+// );
+
+ReactDOM.render(
+  <BrowserRouter>
     <App />
-  </Router>,
-  // document.getElementB›yId('root')
+  </BrowserRouter>,
+  document.getElementById('root')
 );
 
 reportWebVitals();
