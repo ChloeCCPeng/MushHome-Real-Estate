3659cc15db19d36c40f23098530289dda8a85212
Chloe <chloepeng331@icloud.com>
2022-05-03 11:33:01 -0400

    house listing

--- client/src/components/Listing.js
+++ client/src/components/Listing.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import ListingCard from 'components/ListingCard'
 
-function Listing() {
+function Listing({ houses }) { 
+  const houseComponent = houses.map((house) => <ListingCard key={house.id} house={house})
   return (
     <div>
-      
+      <h2>House Listing</h2>
+      <div>
+        {houseComponent}
+      </div>
     </div>
   )
 }
