6c78b3feee4a7e20947c82e411856834d43f0ff5
Chloe Peng <61214246+ChloeCCPeng@users.noreply.github.com>
2022-05-02 20:09:50 -0400

    Revert "Front end"

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,57 @@
+// import React, { useEffect, useState } from "react";
+// import { BrowserRouter, Routes, Route, useHistory } from "react-router-dom";
+// // import NavBar from "./NavBar";
+// import Login from "./components/Login";
+// import Home from "./components/Home";
+
+
+// function App() {
+    
+//     const [user, setUser] = useState(null);
+//     // const [errors, setErrors] = useState([])
+  
+//     // const history = useHistory()
+  
+  
+//     // useEffect(() => {
+//     //   // auto-login
+//     //   fetch("/me").then((r) => {
+//     //     if (r.ok) {
+//     //       r.json().then((user) => setUser(user));
+//     //     };
+//     //   });
+//     // }, []);
+  
+//     // if (!user) return <Login onLogin={setUser} />;
+  
+// return (
+//   <>
+//     <Routes>
+//       <Route exact path="/">
+//         <Home /> 
+//       </Route>
+//       <Route exact path="/login">
+//         <Login onLogin={setUser} />
+//       </Route>
+//       <Route exact path="/account">
+//         <Account /> 
+//       </Route>
+//       <Route exact path="/listing">
+//         <Listing /> 
+//       </Route>
+//       <Route exact path="/houseMarketReport">
+//         <HouseMarketReport /> 
+//       </Route>
+//       {/* <Route exact path="/search">
+//         <Search /> 
+//       </Route> */}
+//     </Routes>
+//   </>
+// );
+//   };
+//   export default App;
+
+// import React, { useEffect, useState } from "react";
 import React, { useEffect, useState } from "react";
 import { Routes, Route, useHistory } from "react-router-dom";
 import Home from "./components/Home";
