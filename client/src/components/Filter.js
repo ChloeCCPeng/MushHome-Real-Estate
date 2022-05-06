d825890b8532f4c763d2b1e993136d3316a23f8f
Chloe <chloepeng331@icloud.com>
2022-05-06 13:16:57 -0400

    mui installed

--- client/src/components/Filter.js
+++ client/src/components/Filter.js
@@ -1,6 +1,10 @@
 import React from 'react'
 
 function Filter() {
+
+  function handleLocationChange(selectedLocation) {
+    onLocationChange(event.target.value)
+}
   return (
     <div>Filter</div>
   )
