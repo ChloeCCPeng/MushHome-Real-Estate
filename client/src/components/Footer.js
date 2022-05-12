b202a5494f8fd224553c2f90eee82c68181cc105
Chloe <chloepeng331@icloud.com>
2022-05-11 23:17:59 -0400

    working project

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -4,17 +4,14 @@ function Footer() {
   return (
     <div>
         <div> 
-        <h1>Footer</h1>
-            <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/chloepeng-software-developer/" class="text-5xl" >LIKE THE IDEA? LET'S CONNECT <span class="material-icons">
-            arrow_forward
-            </span><i class="fa-brands fa-linkedin"></i></a> 
-            
-        </div>
+          <h1>Footer</h1>
+              <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/chloepeng-software-developer/" class="text-5xl" >LIKE THE IDEA? LET'S CONNECT <span class="material-icons">
+              arrow_forward
+              </span><i class="fa-brands fa-linkedin"></i></a> 
+          </div>
         <div>
             {/* <a href="#" class="fa fa-facebook"></a> */}
-
         </div>
-    
     </div>
   )
 }
