a6d8854246205332134fd585b85bc5e1a71cc548
Chloe <chloepeng331@icloud.com>
2022-05-12 10:17:59 -0400

    footer info

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -8,22 +8,22 @@ function Footer() {
           {/* <h1>Footer</h1>
               <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/chloepeng-software-developer/" class="text-5xl" >LIKE THE IDEA? LET'S CONNECT </a>  */}
 
-          <div class="text-white bg-yellow-400 border-none rounded-tl-full text-xs w-3/4 text-center py-6 whitespace-nowrap">
+          <div class="text-white bg-yellow-400 border-none rounded-tl-full text-xs w-3/4 text-center pt-10 pb-5 whitespace-nowrap">
+            <h1 class="text-black">Services </h1>
             <h1 class="text-black">Pre-Sale FAQs</h1>
-            <h1 class="text-black">Services</h1>
             <h1 class="text-black">Legal Documents</h1>
           </div>
-          <div class="text-white bg-yellow-400 border-none text-xs w-3/4 text-center py-6 whitespace-nowrap">
+          <div class="text-white bg-yellow-400 border-none text-xs w-3/4 text-center pt-10 pb-5 whitespace-nowrap">
             <h1 class="text-black">About Us </h1>
             <h1 class="text-black">Contact U</h1>
             <h1 class="text-black">Legal Documents</h1>
           </div>
-          <div class="text-black bg-yellow-400 border-none text-xs w-3/4 text-center pt-10 pb-5 whitespace-nowrap">
-            <h1 class="text-black">Web App Design: Chloe Peng</h1>
-            <h1 class="text-black">Logo Design: Jen Peng</h1>
-            <h1 class="text-black">© 2022 Chloe Peng.  All rights reserved.</h1>
-          </div>
+
         </div>
+        <div class="text-black bg-yellow-400 text-center text-xs pb-2">
+        
+            <h1 class="text-black">Web App Design: Chloe Peng   |   Logo Design: Jen Peng   |   © 2022 Chloe Peng.  All rights reserved.</h1>
+          </div>
 
     </div>
   )
